e16d44de03d84ae529c9efc0d477214e82ce2500
Izaque Firmino <jureggunsk@gmail.com>
2026-09-14 11:32:18 -0300

    aula 12

---
+++ aula_12/exercicio/script.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt("Digite o primeiro número:"));
+let numero2 = Number(prompt("Digite o segundo número:"));
+
+console.log("A soma é " + (numero1 + numero2) + ".");
+console.log("A subtração é " + (numero1 - numero2) + ".");
+console.log("A multiplicação é " + (numero1 * numero2) + ".");
+console.log("A divisão é " + (numero1 / numero2) + ".");
---
+++ aula_12/script.js
@@ -0,0 +1,10 @@
+console.log("Este é do arquivo script.js.");
+let nome = prompt("Digite seu nome:");
+console.log(nome);
+console.log("Olá,"  + nome + "!");
+
+let variavelString = "Sou uma string"
+let variavelNumber = "10"
+let variavelFalseNumber = "Sou uma string"
+let variavelBoolean = true
+ 
