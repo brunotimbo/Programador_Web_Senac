f5b906ff8e46f15d349182e5921d40848f01df93
Izaque Firmino <jureggunsk@gmail.com>
2026-09-22 11:24:55 -0300

    aula_18

--- aula_14/exercicio_02/script.js
+++ aula_14/exercicio_02/script.js
@@ -1,13 +1,21 @@
-let nota1 = document.getElementById("nota1").value;
-const nota2 = document.getElementById("nota2").value;
-const nota3 = document.getElementById("nota3").value;
-const nota4 = document.getElementById("nota4").value;
-
-n1 = parseFloat(nota1);
+let textoResultado = document.getElementById("resultado");
 
 function calcularMedia() {
-    console.log(nota1);
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
+    let nota4 = Number(document.getElementById("nota4").value);
+    
+    media = (nota1 + nota2 + nota3 + nota4)/4
+
+    if (media >= 7) {
+        textoResultado.innerText = `A média é ${media}. Parabéns!`;
+    }
+    else {
+        textoResultado.innerText = `A média é ${media}. Estude mais!`;
+    }
+    
 }
 
-//media = (nota1 + nota2 + nota3 + nota4)/4
+
 
